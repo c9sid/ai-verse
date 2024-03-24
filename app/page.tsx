a07b7b9336449c097f26282d4b8e8ba60cbe9c53
c9sid <kumarsiddharth7666@gmail.com>
2024-03-24 23:56:34 +0530

    minor change

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="h-screen px-5" id="trending">
+      {/* <section className="h-screen px-5" id="trending">
         <div className="flex items-center gap-x-1 pt-14 pb-5">
         <div><h2 className="text-2xl text-red-500 font-semibold">Image Generator</h2></div>
         <div className="text-2xl text-red-500 font-semibold"><FaHotjar /></div>
@@ -46,7 +46,7 @@ export default function Home() {
         <div className="flex overflow-x-scroll no-scrollbar">
         <Card />
         </div>
-      </section>
+      </section> */}
     </>
   );
 }
