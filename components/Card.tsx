a2c9418e6c52601ba3b2a6c177f0867cfbd7e1c3
c9sid <kumarsiddharth7666@gmail.com>
2024-03-24 16:57:01 +0530

    title change

---
+++ components/Card.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+import cardData from '@/app/lib/data'
+
+const Card = () => {
+  return (
+    <>
+      {cardData.map((card:any) => (
+        <div className='card-group flex flex-col w-[300px] min-w-[300px] m-3 px-3 py-5 duration-200 rounded-md hover:shadow-xl bg-slate-900' key={card.id}>
+        <div className="heading flex items-center justify-between pb-3">
+        <h2 className='text-xl font-semibold text-slate-300' key={card}>{card.heading}</h2>
+        <Image 
+          src={card.image}
+          width={20}
+          height={20}
+          alt={'png'}
+        />
+        </div>
+        <p className='pb-3 grow text-slate-500 line-clamp-5 overflow-y-scroll no-scrollbar'>{card.desc}</p>
+        <Link href={card.url} target='_sid' className='text-blue-700 hover:underline font-semibold'>{card.urlText} <span>&rarr;</span></Link>
+      </div>
+      ))}
+    </>
+  )
+}
+
+export default Card
---
+++ components/Navbar.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import Link from 'next/link'
+import React from 'react'
+import { CiMenuFries } from "react-icons/ci";
+import { AiOutlineClose } from "react-icons/ai";
+import { useState } from 'react';
+
+const Navbar = () => {
+
+  const [navbar, setNavbar] = useState(false);
+
+  return (
+<>
+    {/* Desktop navbar */}
+    <nav id='navbar' className='hidden bg-slate-950/60 backdrop-blur-sm sticky top-0 md:flex justify-between items-center px-5 py-5 z-10'>
+      <div className="logo">
+        <Link href={'/'} className='text-2xl font-semibold text-blue-700'>AI verse</Link>
+      </div>
+      <div className="menu">
+        <ul className='flex justify-between items-center gap-x-5'>
+          <li className='text-slate-300 transition duration-200 hover:text-blue-700'><Link href={'/'}>Home</Link></li>
+          <li className='text-slate-300 transition duration-200 hover:text-blue-700'><Link href={'/products'}>Products</Link></li>
+          <li className='text-slate-300 transition duration-200 hover:text-blue-700'><Link href={'/text-to-voice'}>Text to voice</Link></li>
+          <li className='text-slate-300 transition duration-200 hover:text-blue-700'><Link href={'/about'}>About</Link></li>
+          <li className='text-slate-300 transition duration-200 hover:text-blue-700'><Link href={'/contact'}>Contact</Link></li>
+        </ul>
+      </div>
+    </nav>
+
+    {/* Mobile Navbar */}
+    <nav id='navbar' className='flex md:hidden bg-slate-950/60 backdrop-blur-sm sticky top-0 justify-between items-center px-5 py-3 lg:hidden sm:flex z-10'>
+    <div>
+      <Link href={'/'} className='text-xl font-semibold text-blue-700'>AI verse</Link>
+    </div> 
+    <button onClick={() => setNavbar(!navbar)}>
+    {navbar ? (<AiOutlineClose className='text-white' />) : (<CiMenuFries className='text-lg text-white' />)}
+    </button>
+  </nav>
+
+<div 
+className={`${navbar ? '' : 'hidden'}`}
+>
+  <div className='flex justify-center items-start'>
+    <div className='h-auto fixed md:hidden w-11/12 py-5 bg-slate-800 shadow-md rounded-md z-10'>
+      <ul className='space-y-2 text-center pb-3'>
+        <li className='hover:bg-gray-200 text-sm mx-2 rounded text-slate-300 hover:text-black py-1'>
+          <Link onClick={() => setNavbar(!navbar)} href={'/'}>Home</Link></li>
+        <li className='hover:bg-gray-200 text-sm mx-2 rounded text-slate-300 hover:text-black py-1'>
+          <Link onClick={() => setNavbar(!navbar)} href={'/products'}>Products</Link></li>
+        <li className='hover:bg-gray-200 text-sm mx-2 rounded text-slate-300 hover:text-black py-1'>
+          <Link onClick={() => setNavbar(!navbar)} href={'/text-to-voice'}>Text to voice</Link></li>
+        <li className='hover:bg-gray-200 text-sm mx-2 rounded text-slate-300 hover:text-black py-1'>
+          <Link onClick={() => setNavbar(!navbar)} href={'/about'}>About</Link></li>
+        <li className='hover:bg-gray-200 text-sm mx-2 rounded text-slate-300 hover:text-black py-1'>
+          <Link onClick={() => setNavbar(!navbar)} href={'/contact'}>Contact</Link></li>
+      </ul>
+      <hr />
+      <div className='flex justify-center items-center pt-3'>
+        <Link onClick={() => setNavbar(!navbar)} href={'/'} className='bg-blue-700 text-sm text-white hover:bg-blue-800 py-1 rounded-md w-11/12 text-center'>Sign Up</Link>
+      </div>
+    </div>
+  </div>
+  </div>
+    </>
+  )
+}
+
+export default Navbar
