da9664d92390747bf22f9db8f571acc9b219bcda
c9sid <kumarsiddharth7666@gmail.com>
2024-04-14 00:12:18 +0530

    no

--- components/Form.tsx
+++ components/Form.tsx
@@ -4,7 +4,7 @@ import { useFormState } from "react-dom";
  
 export async function FormSubmit (prevState: any ,formData: any) {
 
-  const res = await fetch('http://localhost:3000/api/contact',{
+  const res = await fetch('https://ai-verse-next.netlify.app/api/contact',{
     method: "POST",
     body: formData,
   })
